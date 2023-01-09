77cfacaf0fc42a28ccbea12f04b2686901ecdf6b
harshitasao <harshitasao@gmail.com>
2023-01-09 20:47:43 +0530

    update_4

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,39 +1,148 @@
 package handlers
 
 import (
-	"fmt"
-	"io/ioutil"
+	"Microservice-Go/Microservices-Go/data"
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 )
 
-// Hellohandler
-type Hello struct {
+// // Hellohandler
+// type Hello struct {
+// 	l *log.Logger
+// }
+
+// func NewHello(l *log.Logger) *Hello {
+// 	return &Hello{l}
+// }
+
+// // implementing that method on Hello instance(which are interface)
+// func (h *Hello) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+// 	// NOTE:
+// 	// avoid adding/using concret objects inside the handler so that we enough space to add the
+// 	// objects according to the future cases(testability, future database use etc)
+
+// 	h.l.Println("Hello, World!")
+// 	// reading from the body and anything that implements the io.ReadCloser there we need to use
+// 	// go library
+// 	data, err := ioutil.ReadAll(r.Body)
+// 	if err != nil {
+// 		http.Error(rw, "Oops", http.StatusBadRequest)
+// 		return
+// 	}
+// 	// Using log we are getting data in the logs for the developer but not for the user
+// 	// log.Printf("Data %s\n", data)
+
+// 	// data for the user
+// 	fmt.Fprintf(rw, "Hello %s\n", data)
+
+// }
+
+// Products is a http.Handler
+type Products struct {
 	l *log.Logger
 }
 
-func NewHello(l *log.Logger) *Hello {
-	return &Hello{l}
+// NewProducts creates a products handler with the given logger
+func NewProducts(l *log.Logger) *Products {
+	return &Products{l}
 }
 
-// implementing that method on Hello instance(which are interface)
-func (h *Hello) ServerHttp(rw http.ResponseWriter, r *http.Request) {
-	// NOTE:
-	// avoid adding/using concret objects inside the handler so that we enough space to add the
-	// objects according to the future cases(testability, future database use etc)
-
-	h.l.Println("Hello, World!")
-	// reading from the body and anything that implements the io.ReadCloser there we need to use
-	// go library
-	data, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		http.Error(rw, "Oops", http.StatusBadRequest)
+// ServeHTTP is the main entry point for the handler and staisfies the http.Handler
+// interface
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	// handle the request for a list of products
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
 		return
 	}
-	// Using log we are getting data in the logs for the developer but not for the user
-	// log.Printf("Data %s\n", data)
 
-	// data for the user
-	fmt.Fprintf(rw, "Hello %s\n", data)
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
 
+	if r.Method == http.MethodPut {
+		p.l.Println("PUT", r.URL.Path)
+		// expect the id in the URI
+		reg := regexp.MustCompile(`/([0-9]+)`)
+		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
+
+		if len(g) != 1 {
+			p.l.Println("Invalid URI more than one id")
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		if len(g[0]) != 2 {
+			p.l.Println("Invalid URI more than one capture group")
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			p.l.Println("Invalid URI unable to convert to numer", idString)
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		p.updateProducts(id, rw, r)
+		return
+	}
+
+	// catch all
+	// if no method is satisfied return an error
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+// getProducts returns the products from the data store
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle GET Products")
+
+	// fetch the products from the datastore
+	lp := data.GetProducts()
+
+	// serialize the list to JSON
+	err := lp.ToJSON(rw)
+	if err != nil {
+		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST Product")
+
+	prod := &data.Product{}
+
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	data.AddProduct(prod)
+}
+
+func (p Products) updateProducts(id int, rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle PUT Product")
+
+	prod := &data.Product{}
+
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	err = data.UpdateProduct(id, prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(rw, "Product not found", http.StatusInternalServerError)
+		return
+	}
 }
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"Microservice-Go/Microservices-Go/handlers"
+	"context"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/harshitasao/Microservices-go/handlers"
+	"os/signal"
+	"time"
+	// "github.com/harshitasao/Microservices-go/handlers"
 )
 
 func main() {
@@ -16,15 +19,51 @@ func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 
 	// connecting the main with hello handler via this NewHello function
-	hh := handlers.NewHello(l)
+	ph := handlers.NewProducts(l)
 
 	// creating new servemux
 	sm := http.NewServeMux()
 	//implementing handle method of servemux type object
 	// this method takes 2 parameters the path and the handler needs to be working for trhat path
-	sm.Handle("/", hh)
+	sm.Handle("/", ph)
 
 	// creating a basic webserver
 	// and here instead of nil i am writing sm to make the default as my servemux not the defaultServerHttp
-	http.ListenAndServe(":9090", sm)
+	// http.ListenAndServe(":9090", sm)
+
+	// creating new server manually becoz the default one doesnot provide with enough functionality
+
+	// create a new server
+	s := http.Server{
+		Addr:         ":9090",           // configure the bind address
+		Handler:      sm,                // set the default handler
+		ErrorLog:     l,                 // set the logger for the server
+		ReadTimeout:  5 * time.Second,   // max time to read request from the client
+		WriteTimeout: 10 * time.Second,  // max time to write response to the client
+		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
+	}
+
+	// start the server
+	go func() {
+		l.Println("Starting server on port 9090")
+
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Printf("Error starting server: %s\n", err)
+			os.Exit(1)
+		}
+	}()
+
+	// trap sigterm or interupt and gracefully shutdown the server
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
+
+	// Block until a signal is received.
+	sig := <-c
+	log.Println("Got signal:", sig)
+
+	// gracefully shutdown the server, waiting max 30 seconds for current operations to complete
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(ctx)
 }
